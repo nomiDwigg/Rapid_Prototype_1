96f1220a2f17050c0cac0b37554dc5ac5426a80d
Naomi Wiggins <Naomiwiggins@me.com>
2020-07-08 15:15:12 +1200

    Updated Project

--- Grappling_Hook_Game/Source/Grappling_Hook_Game/Grappling_Hook_Game.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "Grappling_Hook_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Grappling_Hook_Game, "Grappling_Hook_Game" );
- 
